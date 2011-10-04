6ecb5604ec2760f39936bc0bd153fc543a6e16b1
mmasciov <>
2011-10-04 15:30:39 +0000

    This commit was manufactured by cvs2svn to create tag 'MT2_V01-01-01'.
    Sprout from master 2011-10-04 15:30:38 UTC pnef 'a few changes:'
    Delete:
        Code/MT2AnalysisCode/RootMacros/SetStyle_PRD.C
        Code/MT2AnalysisCode/RootMacros/rootlogon.C
        Code/MT2AnalysisCode/RootMacros/run_MassPlots.C
        Code/MT2_cuts/test.dat
        Code/Macros/MT2PostProcessing/MT2PostProcessing.csh
        Code/Macros/MT2PostProcessing/MT2PostProcessing.py
        Code/Macros/MT2PostProcessing/MT2treeSkimming.C

--- Code/MT2AnalysisCode/RootMacros/rootlogon.C
+++
@@ -1,7 +0,0 @@
-void rootlogon() {
-  gSystem->SetIncludePath(" -I../MT2Code/include/");
-  gSystem->SetIncludePath(" -I../TESCO/include/");
-  gSystem->Load("libPhysics");
-  gSystem->Load("../MT2Code/shlib/libDiLeptonAnalysis.so");
-  gROOT->ProcessLine(".x SetStyle_PRD.C");
-}

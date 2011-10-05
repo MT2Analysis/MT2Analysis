e4b5eb38aad1093f6c78cca312befb4f419f3bf6
pnef <>
2011-10-05 08:36:54 +0000

    fixing bug in includes

--- Code/MT2AnalysisCode/RootMacros/rootlogon.C
+++ Code/MT2AnalysisCode/RootMacros/rootlogon.C
@@ -1,6 +1,5 @@
 void rootlogon() {
-  gSystem->SetIncludePath(" -I../MT2Code/include/");
-  gSystem->SetIncludePath(" -I../TESCO/include/");
+  gSystem->SetIncludePath(" -I../MT2Code/include/ -I../TESCO/include/");
   gSystem->Load("libPhysics");
   gSystem->Load("../MT2Code/shlib/libDiLeptonAnalysis.so");
   gROOT->ProcessLine(".x SetStyle_PRD.C");

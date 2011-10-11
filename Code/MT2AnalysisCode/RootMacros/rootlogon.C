dca6c6e2cf673ab0f807cd31c3d12e6109b81d07
mmasciov <>
2011-10-11 21:11:03 +0000

    This commit was manufactured by cvs2svn to create tag 'MT2_V01-01-02'.
    Sprout from master 2011-10-11 21:11:02 UTC pnef 'adding triggers, additional noise flags, nloops for hemispheres'
    Delete:
        Code/MT2AnalysisCode/MT2Code/samples/samples_highMT2.dat
        Code/MT2AnalysisCode/RootMacros/SetStyle_PRD.C
        Code/MT2AnalysisCode/RootMacros/rootlogon.C
        Code/MT2AnalysisCode/RootMacros/run_MT2Shapes.C
        Code/MT2AnalysisCode/RootMacros/run_MassPlots.C
        Code/MT2_cuts/test.dat
        Code/Macros/MT2PostProcessing/MT2PostProcessing.csh
        Code/Macros/MT2PostProcessing/MT2PostProcessing.py
        Code/Macros/MT2PostProcessing/MT2treeSkimming.C

--- Code/MT2AnalysisCode/RootMacros/rootlogon.C
+++
@@ -1,6 +0,0 @@
-void rootlogon() {
-  gSystem->SetIncludePath(" -I../MT2Code/include/ -I../TESCO/include/");
-  gSystem->Load("libPhysics");
-  gSystem->Load("../MT2Code/shlib/libDiLeptonAnalysis.so");
-  gROOT->ProcessLine(".x SetStyle_PRD.C");
-}

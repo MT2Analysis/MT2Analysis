b160d61b8c93d34344859123ea0dbad08ba74b9d
pnef <>
2012-01-04 10:08:40 +0000

    fix to also merge pileup and nevents histos when skimming

--- Code/Macros/MT2PostProcessing/MT2treeSkimming.C
+++ Code/Macros/MT2PostProcessing/MT2treeSkimming.C
@@ -22,27 +22,35 @@ void MT2treeSkimming(string sample, string shlib, string prefix) {
 	// cuts --------------------------------------------
   	std::ostringstream cutStream;
 	cutStream       << " " 	  
-//	  << "misc.MT2 >= 400"                         << "&&" 
-	  << "misc.MET>=30"                            << "&&"
-	  << "misc.HT > 400 "                          << "&&"
-//	  << "misc.caloHT50_ID >600 "                  << "&&"
-	  << "misc.Jet0Pass ==1"                       << "&&"
-	  << "misc.Jet1Pass ==1"                       << "&&"
-	  << "misc.SecondJPt  >100"                    << "&&"
-	  << "misc.PassJetID ==1"                      << "&&"
-	  << "misc.Vectorsumpt < 70"                   << "&&"
-//	  << "misc.MinMetJetDPhi >0.3"                 << "&&"
-	  << "misc.HBHENoiseFlag == 0"                 << "&&"
-	  << "misc.CSCTightHaloID==0"                  << "&&"
-	  << "misc.CrazyHCAL==0"                       << "&&"
-	  << "(NEles==0 || ele[0].lv.Pt()<10)"         << "&&"
-	  << "(NMuons==0 || muo[0].lv.Pt()<10)"        << "&&"
+//	  << "misc.MT2 >=50"                                     << "&&" 
+	  << "misc.MET>=30"                                      << "&&"
+	  << "misc.HT > 750 "                                    << "&&"
+	  << "misc.Jet0Pass ==1"                                 << "&&"
+	  << "misc.Jet1Pass ==1"                                 << "&&"
+	  << "misc.SecondJPt  >100"                              << "&&"
+	  << "misc.PassJetID ==1"                                << "&&"
+	  << "misc.Vectorsumpt < 70"                             << "&&"
+	  << "((misc.MinMetJetDPhi >0.3&&NBJets==0)||NBJets>=1)" << "&&"
+// Lepton Veto
+//	  << "(NEles==0 || ele[0].lv.Pt()<10)"                   << "&&"
+//	  << "(NMuons==0 || muo[0].lv.Pt()<10)"                  << "&&"
+// Lepton Skim
+//	  << "(ele[0].lv.Pt()>10 || muo[0].lv.Pt()>10)"          << "&&"
 // LowMT2 ----------------------------
-//	  << "misc.LeadingJPt >150"                    << "&&"
-//	  << "NBJets >0"                               << "&&"
-//	  << "NJetsIDLoose >=4"                        << "&&"
+//	  << "misc.LeadingJPt >150"                              << "&&"
+//	  << "NBJets >0"                                         << "&&"
+//	  << "NJetsIDLoose >=4"                                  << "&&"
 // -----------------------------------
-	  << "NJetsIDLoose >=3";
+	  << "NJetsIDLoose40 >=3"                                << "&&"
+// Photons
+//          << "(GenZ[0].Pt()>100)"                                << "&&"
+//          << "(GenDiLeptPt(0,10,0,1000,true)>=100||GenPhoton[0].Pt()>=100)"  << "&&"
+//          << "NPhotons >0"                                      << "&&"
+// Noise
+	  << "misc.HBHENoiseFlag == 0"                           << "&&"
+	  << "misc.CSCTightHaloID==0"                            << "&&"
+	  << "misc.CrazyHCAL==0";
+
 	
 	TString basecut = cutStream.str();
 	string  SEL= "("+basecut+")";
@@ -55,6 +63,8 @@ void MT2treeSkimming(string sample, string shlib, string prefix) {
 	// files ---------------------------------------
 	TFile *_file0 = TFile::Open( (file).c_str()); 
 	TTree * t = (TTree*) _file0->Get("MassTree");
+	TH1F*   h_PUWeights = (TH1F*) _file0->Get("h_PUWeights");
+	TH1F*   h_Events    = (TH1F*) _file0->Get("h_Events");
 	t->SetMaxTreeSize(19000000000);
 	TFile*out = TFile::Open( (outfile).c_str(),"RECREATE");
 	TTree *tc = t->CopyTree(SEL.c_str());   
@@ -62,6 +72,14 @@ void MT2treeSkimming(string sample, string shlib, string prefix) {
 	f_log << "skimmed tree has " << nentries << " entries." <<endl;
 	f_log.close();
 	out->Write();
+	if(h_PUWeights!=0){
+		cout << "writing TH1F h_PUWeights" << endl;
+		h_PUWeights->Write();
+	}
+	if(h_Events!=0){
+		cout << "writing TH1F h_Events" << endl;
+		h_Events->Write();
+	}
 	out->Close();
 	_file0->Close();
 	cout << "Result file: " << outfile << endl;

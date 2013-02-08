e98eb077e43274bcc1068c833c6aeae90a9effee
mmasciov <>
2013-02-08 16:00:22 +0000

    TriggerSkim + Merging

--- Code/Macros/MT2PostProcessing/MT2treeSkimming.C
+++ Code/Macros/MT2PostProcessing/MT2treeSkimming.C
@@ -45,10 +45,10 @@ void MT2treeSkimming(string sample, string shlib, string prefix) {
 //	  << "NJetsIDLoose >=4"                                  << "&&"
 // -----------------------------------
 //	  << "NJetsIDLoose40 >=3"                                << "&&"
-// Photons
-	  << "(GenDiLeptPt(0,10,0,1000,false)>=100||GenPhoton[0].Pt()>=100)"  << "&&"
-//	  << "(RecoOSDiLeptPt(10,10,0,10000)>=100 ||photon[0].lv.Pt()>=100)"  << "&&"
-//	  << "NPhotons >0"  
+// // Photons
+// 	  << "(GenDiLeptPt(0,10,0,1000,false)>=100||GenPhoton[0].Pt()>=100)"  << "&&"
+// //	  << "(RecoOSDiLeptPt(10,10,0,10000)>=100 ||photon[0].lv.Pt()>=100)"  << "&&"
+// //	  << "NPhotons >0"  
 // Noise
 //	  << "misc.HBHENoiseFlag == 0"                           << "&&"
 //	  << "misc.CSCTightHaloID==0"                            << "&&"

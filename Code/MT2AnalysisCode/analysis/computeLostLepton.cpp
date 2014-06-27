f6666304d67117175bc00065b0b2f387e1eff7d8
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-06-27 16:54:22 +0200

    working version with all samples and all signal regions. now have to check if results are ok

--- Code/MT2AnalysisCode/analysis/computeLostLepton.cpp
+++ Code/MT2AnalysisCode/analysis/computeLostLepton.cpp
@@ -21,19 +21,31 @@ bool fIncludeTaus = true;
 
 
 MT2LostLeptonEstimate* computeLostLepton( const MT2Sample& sample, std::vector<MT2HTRegion> HTRegions, std::vector<MT2SignalRegion> signalRegions );
-//void computeSignalRegionEstimate( TFile* outfile, const MT2Sample& sample, const std::string& leptType, int njet_min, int njet_max, int nbjet_min, int nbjet_max, float ht_min, float ht_max );
 MT2LeptonTypeLLEstimate* getLeptonTypeLLEstimate( const std::string& leptType, TTree* tree, MT2Sample sample, std::vector<MT2HTRegion> HTRegions, std::vector<MT2SignalRegion> signalRegions );
 //float getISRCorrection( MT2tree* fMT2tree, const MT2Sample& sample, const std::string& sampletype );
 //void getBTagSF( int njets, int nbjets, double& btagSF, double& btagSFerr );
 MT2LostLeptonEstimate* mergeEstimates( std::vector<MT2LostLeptonEstimate*> llest, const std::string& n1, const std::string& n2="", const std::string& n3="", const std::string& n4="", const std::string& n5="" );
+std::vector<TH1D*> getPredictionHistos( const std::string& prefix, const std::string& leptType, std::vector<MT2HTRegion> HTRegions, std::vector<MT2SignalRegion> signalRegions, MT2LostLeptonEstimate* ll_tot, MT2LostLeptonEstimate* ll_bg, MT2LostLeptonEstimate* ll_eff );
 
 
 
-int main() {
+
+int main( int argc, char* argv[] ) {
 
 
+  if( argc!=2 ) {
+    std::cout << "USAGE: ./computeLostLepton [samplesFileName]" << std::endl;
+    std::cout << "Exiting." << std::endl;
+    exit(11);
+  }
 
-  std::string samplesFileName = "samples_HT_filter_prova_fast.dat";
+
+  std::string sampleName(argv[1]);
+
+  std::string samplesFileName = "samples/samples_" + sampleName + ".dat";
+  std::cout << std::endl << std::endl;
+  std::cout << "-> Loading samples from file: " << samplesFileName << std::endl;
+
   std::vector<MT2Sample> fSamples = MT2Common::loadSamples(samplesFileName);
 
  
@@ -43,45 +55,68 @@ int main() {
   HTRegions.push_back(MT2HTRegion("highHT",  1200., 100000.,  30.));
 
   std::vector<MT2SignalRegion> signalRegions;
-  signalRegions.push_back(MT2SignalRegion(3, 3, 1, 1));  // 3j1b
   signalRegions.push_back(MT2SignalRegion(2, 2, 0, 0));  // 2j0b
-  signalRegions.push_back(MT2SignalRegion(3, 3, 0, 0));  // 3j0b
+  signalRegions.push_back(MT2SignalRegion(2, 2, 1, 2));  // 2j1to2b
+  signalRegions.push_back(MT2SignalRegion(-1, -1, 3, -1));  // 3b
+  signalRegions.push_back(MT2SignalRegion(3, 5, 0, 0));  // 3to5j0b
+  signalRegions.push_back(MT2SignalRegion(3, 5, 1, 1));  // 3to5j1b
+  signalRegions.push_back(MT2SignalRegion(3, 5, 2, 2));  // 3to5j2b
+  signalRegions.push_back(MT2SignalRegion(6, 6, 0, 0));  // 6j0b
+  signalRegions.push_back(MT2SignalRegion(6, 6, 1, 1));  // 6j1b
+  signalRegions.push_back(MT2SignalRegion(6, 6, 2, 2));  // 6j2b
 
 
-  TFile* outfile = TFile::Open("outfile.root", "recreate");
+
   
   std::vector<MT2LostLeptonEstimate*> llest;
   for( unsigned i=0; i<fSamples.size(); ++i )
     llest.push_back( computeLostLepton( fSamples[i], HTRegions, signalRegions ) );
   
 
+  std::cout << "-> Done looping on samples. Start merging." << std::endl;
+
   MT2LostLeptonEstimate* ll_data  = mergeEstimates( llest, "HT-Data" );
   MT2LostLeptonEstimate* ll_top   = mergeEstimates( llest, "Top" );
-  //MT2LostLeptonEstimate* ll_qcd   = mergeEstimates( llest, "QCD" );
-  //MT2LostLeptonEstimate* ll_wjets = mergeEstimates( llest, "Wtolnu" );
-  //MT2LostLeptonEstimate* ll_other = mergeEstimates( llest, "DY", "VV" );
+  MT2LostLeptonEstimate* ll_qcd   = mergeEstimates( llest, "QCD" );
+  MT2LostLeptonEstimate* ll_wjets = mergeEstimates( llest, "Wtolnu" );
+  MT2LostLeptonEstimate* ll_other = mergeEstimates( llest, "DY", "VV" );
   
-  //MT2LostLeptonEstimate* ll_topW   = new MT2LostLeptonEstimate(*ll_top  + *ll_wjets);
-  //MT2LostLeptonEstimate* ll_bg     = new MT2LostLeptonEstimate(*ll_qcd  + *ll_other);
-  //MT2LostLeptonEstimate* ll_allMC  = new MT2LostLeptonEstimate(*ll_topW + *ll_bg);
+  MT2LostLeptonEstimate* ll_topW   = new MT2LostLeptonEstimate(*ll_top  + *ll_wjets);
+  MT2LostLeptonEstimate* ll_bg     = new MT2LostLeptonEstimate(*ll_qcd  + *ll_other);
+  MT2LostLeptonEstimate* ll_allMC  = new MT2LostLeptonEstimate(*ll_topW + *ll_bg);
 
-  MT2Region r( &HTRegions[0], &signalRegions[0] );
+  std::cout << "-> Done merging. Start computing preditions." << std::endl;
 
-  std::cout << "yields:" << std::endl;
-  std::cout << llest[1]->l["Ele"] << std::endl;
-  std::cout << llest[1]->l["Ele"]->getRecoRegion(r.getName()) << std::endl;
-  std::cout << llest[1]->l["Ele"]->getRecoRegion(r.getName())->yield << std::endl;
-  std::cout << "all: " << llest[1]->l["Ele"]->getRecoRegion(r.getName())->yield->Integral() << std::endl;
-  std::cout << ll_top->l["Ele"] << std::endl;
-  std::cout << ll_top->l["Ele"]->getRecoRegion(r.getName()) << std::endl;
-  std::cout << ll_top->l["Ele"]->getRecoRegion(r.getName())->yield << std::endl;
-  std::cout << "top: " << ll_top->l["Ele"]->getRecoRegion(r.getName())->yield->Integral() << std::endl;
-  //std::cout << "data: " << ll_data->l["Ele"]->getRecoRegion(r.getName())->yield->Integral() << std::endl;
 
-  // then create summary histos and write to outfile
-//  std::vector<TH1D*> histos_ele_reco = createPredictionHistos("Prediction"     , "Ele", llest );
-//  std::vector<TH1D*> histos_gen  = createPredictionHistos("SimulationTruth", leptType, HTRegions, signalRegions);
+  std::vector<TH1D*> vh1_data_ele = getPredictionHistos( "Prediction", "Ele", HTRegions, signalRegions, ll_data, ll_bg, ll_topW );
+  std::vector<TH1D*> vh1_data_mu  = getPredictionHistos( "Prediction", "Muo", HTRegions, signalRegions, ll_data, ll_bg, ll_topW );
 
+  std::vector<TH1D*> vh1_mc_ele   = getPredictionHistos( "SimulationTruth", "Ele", HTRegions, signalRegions, ll_allMC, ll_bg, ll_topW );
+  std::vector<TH1D*> vh1_mc_mu    = getPredictionHistos( "SimulationTruth", "Muo", HTRegions, signalRegions, ll_allMC, ll_bg, ll_topW );
+
+
+
+  std::string outputdir = "LostLeptonFiles";
+  system(Form("mkdir -p %s", outputdir.c_str()));
+
+  
+  TFile* outfile = TFile::Open(Form("%s/ll_%s.root", outputdir.c_str(), sampleName.c_str()), "recreate");
+  outfile->cd();
+
+  for( unsigned i=0; i<HTRegions.size(); ++i ) {
+
+    vh1_data_ele[i]->Write();
+    vh1_data_mu [i]->Write();
+
+    vh1_mc_ele  [i]->Write();
+    vh1_mc_mu   [i]->Write();
+
+  }
+
+
+  outfile->Close();
+
+  std::cout << "-> Done computing preditions. Written to file: " << outfile->GetName() << std::endl;
 
   return 0;
 
@@ -96,9 +131,9 @@ MT2LostLeptonEstimate* computeLostLepton( const MT2Sample& sample, std::vector<M
   std::cout << std::endl << std::endl;
   std::cout << "-> Starting computation for sample: " << sample.name << std::endl;
 
-  //definition of MT2tree
-  MT2tree* fMT2tree = new MT2tree();
-  sample.tree->SetBranchAddress("MT2tree", &fMT2tree);
+  TFile* file = TFile::Open(sample.file.c_str());
+  TTree* tree = (TTree*)file->Get("MassTree");
+
 
 
   std::ostringstream preselectionStream;
@@ -155,8 +190,10 @@ MT2LostLeptonEstimate* computeLostLepton( const MT2Sample& sample, std::vector<M
   TString cuts_ele = cuts + " && NEles==1 && NMuons==0";
   TString cuts_muo = cuts + " && NEles==0 && NMuons==1";
 
-  TTree* tree_ele = sample.tree->CopyTree(cuts_ele);
-  TTree* tree_muo = sample.tree->CopyTree(cuts_muo);
+  gROOT->cd();
+
+  TTree* tree_ele = tree->CopyTree(cuts_ele);
+  TTree* tree_muo = tree->CopyTree(cuts_muo);
 
 
   MT2LeptonTypeLLEstimate* llest_ele = getLeptonTypeLLEstimate( "Ele", tree_ele, sample, HTRegions, signalRegions );
@@ -165,6 +202,11 @@ MT2LostLeptonEstimate* computeLostLepton( const MT2Sample& sample, std::vector<M
   MT2LostLeptonEstimate* llest = new MT2LostLeptonEstimate(sample.name, sample.sname);
   llest->l["Ele"] = llest_ele;
   llest->l["Muo"] = llest_muo;
+
+  delete tree;
+  delete tree_ele;
+  delete tree_muo;
+  file->Close();
 
   return llest;
 
@@ -199,7 +241,7 @@ MT2LeptonTypeLLEstimate* getLeptonTypeLLEstimate( const std::string& leptType, T
 
 
   MT2tree* fMT2tree = new MT2tree();
-  sample.tree->SetBranchAddress("MT2tree", &fMT2tree);
+  tree->SetBranchAddress("MT2tree", &fMT2tree);
 
   int nentries = tree->GetEntries();
 
@@ -233,7 +275,7 @@ MT2LeptonTypeLLEstimate* getLeptonTypeLLEstimate( const std::string& leptType, T
 
         MT2Region thisRegion( &HTRegions[iHT], &signalRegions[iSR] );
 
-        MT2SingleLLEstimate* thisRecoEst = llest->getRecoRegion( thisRegion.getName() );
+        MT2SingleLLEstimate* thisRecoEst = llest->getRegion( thisRegion.getName() );
 
         thisRecoEst->yield->Fill( mt2, weight );
 
@@ -296,7 +338,12 @@ MT2LostLeptonEstimate* mergeEstimates( std::vector<MT2LostLeptonEstimate*> llest
 
 
 
-std::vector<TH1D*> createPredictionHistos( const std::string& prefix, const std::string& leptType, std::vector<MT2HTRegion> HTRegions, std::vector<MT2SignalRegion> signalRegions) {
+
+
+
+
+std::vector<TH1D*> getPredictionHistos( const std::string& prefix, const std::string& leptType, std::vector<MT2HTRegion> HTRegions, std::vector<MT2SignalRegion> signalRegions, MT2LostLeptonEstimate* ll_tot, MT2LostLeptonEstimate* ll_bg, MT2LostLeptonEstimate* ll_eff ) {
+
 
   int nHistos = HTRegions.size();
   int nBins = signalRegions.size();
@@ -309,8 +356,25 @@ std::vector<TH1D*> createPredictionHistos( const std::string& prefix, const std:
     TH1D* h1 = new TH1D(Form("%s_%s_%s", prefix.c_str(), leptType.c_str(), HTRegions[i].name.c_str()), "", nBins, 0., nBins);
     h1->Sumw2();
 
-    for( unsigned iBin=1; iBin<(nBins+1); ++iBin ) 
-      h1->GetXaxis()->SetBinLabel(iBin, signalRegions[iBin].getName().c_str());
+    for( unsigned j=0; j<nBins; ++j ) {
+  
+      int iBin = j+1;
+
+      MT2Region thisRegion( &HTRegions[i], &signalRegions[j] );
+
+      h1->GetXaxis()->SetBinLabel(iBin, signalRegions[j].getName().c_str());
+
+      float totYield = (ll_tot->l[leptType.c_str()]!=0) ? ll_tot->l[leptType.c_str()]->getRegion(thisRegion.getName())->yield->Integral() : 0.;
+      float  bgYield = (ll_bg ->l[leptType.c_str()]!=0) ? ll_bg ->l[leptType.c_str()]->getRegion(thisRegion.getName())->yield->Integral() : 0.;
+
+      float  effMT   = (ll_eff->l[leptType.c_str()]!=0) ? ll_eff->l[leptType.c_str()]->getRegion(thisRegion.getName())->effMT()  ->GetEfficiency(1) : 0.;
+      float  effLept = (ll_eff->l[leptType.c_str()]!=0) ? ll_eff->l[leptType.c_str()]->getRegion(thisRegion.getName())->effLept()->GetEfficiency(1) : 0.;
+
+      float pred = (effLept>0. && effMT>0.) ? (totYield-bgYield)*(1.-effLept)/(effLept*effMT) : 0.;
+
+      h1->SetBinContent( iBin, pred );
+
+    }
 
     histos.push_back(h1);
 
@@ -324,176 +388,10 @@ std::vector<TH1D*> createPredictionHistos( const std::string& prefix, const std:
 
 
 
-    
+
+
+
 /*
-
-void computeSignalRegionEstimate( TFile* outfile, MT2Sample sample, int njet_min, int njet_max, int nbjet_min, int nbjet_max, float ht_min, float ht_max ) {
-
-  bool isMET = (ht_min==450.);
-
-  std::string signal_region = MT2Common::getSignalRegion(ht_min, njet_min, njet_max, nbjet_min, nbjet_max);
-
-  int nBins;
-  Double_t *bins;
-  MT2Common::getBins(signal_region, nBins, bins);
-
-
-  std::string hs = "_" + leptType + "_" + signal_region + "_" + sample.type;
-
-  std::string histoName = "RecoLepEvents" + hs;
-  TH1D* histo = new TH1D( histoName.c_str(), "", nBins, bins );
-  histo->Sumw2();
-
-
-
-
-  std::ostringstream cutStream;
-  cutStream << " " 
-    << "NTausIDLoose3Hits==0"                   << "&&"
-    << "misc.Jet0Pass ==1"                      << "&&"
-    << "misc.Jet1Pass ==1"                      << "&&"
-    << "misc.Vectorsumpt < 70"                  << "&&" 
-    << "misc.MinMetJetDPhi4Pt40 >0.3"           << "&&"
-    << "misc.HT>" << ht_min << "&& misc.HT<=" << ht_max;
-  if( isMET ) cutStream << "&&misc.MET>200.";
-  else        cutStream << "&&misc.MET>30.";
-  if( fFast ) cutStream << "&&misc.MT2>=100.";//lowest border in MT2
-  
-  std::ostringstream cutStreamBase;
-  cutStreamBase << " " 
-    << "misc.PassJet40ID ==1"                             << "&&"
-    << "(misc.HBHENoiseFlag == 0 || misc.ProcessID==10)"  << "&&" // signal samples (fastsim) do not have this filter
-    << "misc.CSCTightHaloIDFlag == 0"                     << "&&"
-    << "misc.trackingFailureFlag==0"                      << "&&"
-    << "misc.eeBadScFlag==0"                              << "&&"
-    << "misc.EcalDeadCellTriggerPrimitiveFlag==0"         << "&&"
-    << "misc.TrackingManyStripClusFlag==0"                << "&&"
-    << "misc.TrackingTooManyStripClusFlag==0"             << "&&"
-    << "misc.TrackingLogErrorTooManyClustersFlag==0"      << "&&"
-    << "misc.CrazyHCAL==0";
-  cutStreamBase << "&&misc.MET/misc.CaloMETRaw<=2.";//HO cut
-  
-
-  std::ostringstream triggerStream;
-  if( isMET ) {
-
-    triggerStream << "( ( ("
-      << "trigger.HLT_PFMET150_v2 == 1 || trigger.HLT_PFMET150_v3 == 1 || trigger.HLT_PFMET150_v4 == 1 || "
-      << "trigger.HLT_PFMET150_v5 == 1 || trigger.HLT_PFMET150_v6 == 1 || trigger.HLT_PFMET150_v7 == 1 )"
-      << "||("
-      << "trigger.HLT_PFHT350_PFMET100_v3==1 || trigger.HLT_PFHT350_PFMET100_v4==1 || trigger.HLT_PFHT350_PFMET100_v5==1 || "
-      << "trigger.HLT_PFHT350_PFMET100_v6==1 || trigger.HLT_PFHT350_PFMET100_v7==1 || trigger.HLT_PFNoPUHT350_PFMET100_v1==1 || "
-      << "trigger.HLT_PFNoPUHT350_PFMET100_v3==1 || trigger.HLT_PFNoPUHT350_PFMET100_v4==1 ) ) &&TOBTECTagger<=8&&ExtraBeamHaloFilter==0)";
-
-  } else  {
-
-    triggerStream << "( ("
-      << "trigger.HLT_PFHT650_v5 == 1 || trigger.HLT_PFHT650_v6 == 1 || trigger.HLT_PFHT650_v7 == 1 || "
-      << "trigger.HLT_PFHT650_v8 == 1 || trigger.HLT_PFHT650_v9 == 1 || "
-      << "trigger.HLT_PFNoPUHT650_v1 == 1 || trigger.HLT_PFNoPUHT650_v3 == 1 || trigger.HLT_PFNoPUHT650_v4 == 1) &&TOBTECTagger<=8&&ExtraBeamHaloFilter==0)";
-
-  }
-
-  TString cuts = cutStream.str().c_str();
-  TString basecuts = cutStreamBase.str().c_str();
-  TString trigger = triggerStream.str().c_str();
-
-
-  Long64_t nbytes = 0, nb = 0;
-  int nev =0;
-
-  TString myCuts = cuts + "&&" + basecuts;
-  if( sample.type=="data") myCuts += " && " + trigger; //cuts to be aplied only on data
- 
-  std::cout << "Cuts for Flow: " << myCuts << std::endl;
-  TTree* tree_sel = sample.tree->CopyTree(myCuts);
- 
-  Long64_t nentries = tree_sel->GetEntries();
-
-  std::cout << "Filtering done, size=" << nentries  << std::endl;
-
-  //run only over events passing event selection
-  for( unsigned jentry=0; jentry<nentries; ++jentry ) {
-
-    tree_sel->GetEntry(jentry); 
-
-    if ( jentry % 100000 == 0 )  std::cout << "+++ Proccessing event " << jentry << " / " << nentries << std::endl;
-
-    Double_t weight = sample_weight;
-    bool isData = fMT2tree->misc.isData;
-
-    if ( !(isData) ) weight = weight * fMT2tree->pileUp.Weight; // pile-up reweighting for MC 
-
-    float ht = fMT2tree->misc.HT;
-    float mt2 = fMT2tree->misc.MT2;
-
-    std::string sampletype = MT2Common::getSampleType(sample);
-    if(ht>=750. && ht<1200. && fMT2tree->NJetsIDLoose40==2 && fMT2tree->NBJets40CSVM>=1 && mt2 >=135.&& mt2 <=170. && sampletype=="SingleTop" && fMT2tree->misc.LumiSection==149 && fMT2tree->misc.Event==44699) continue;//sample T_t_highHT - fix for one 8 TeV MC sample with crazy weight
-
-    //the if clause below computes the weight due to the 'ISR recipe'
-    Double_t ISRweight(1.); 
-    Double_t weightnoISR = weight;
-    if(fISRreweight && !isData) ISRweight = getISRCorrection(fMT2tree, sample, sampletype);
-  
-  
-    int nele = fMT2tree->NEles;
-    int nmu  = fMT2tree->NMuons;
-    Bool_t recoedele       = (nele ==1 && nmu ==0 && fMT2tree->ele[0].MT<100. );
-    Bool_t recoedmuo       = (nmu  ==1 && nele==0 && fMT2tree->muo[0].MT<100. );
-    Bool_t recoedelenomt   = (nele ==1 && nmu ==0                             );
-    Bool_t recoedmuonomt   = (nmu  ==1 && nele==0                             );
-    Bool_t norecolep       = (nmu  ==0 && nele==0                             );
-
-    std::string slep;
-    if(recoedele) slep = "_Ele";
-    if(recoedmuo) slep = "_Muo";
-
-    std::string sHT;
-    if( isMET ){
-      if( ht<=450. )      sHT = "_HTge0";
-      else if( ht<=750. ) sHT = "_HTge450";
-    } else {
-      if( ht>1200. )      sHT = "_HTge1200";
-      else if( ht>750. )  sHT = "_HTge750";
-    }
-
-
-    //get BTV weight
-    double btagSF(1.), btagSFerr(0.);
-    if(!isData && fbTagReweight)
-      getBTagSF( fMT2tree->NJetsIDLoose40, fMT2tree->NBJets40CSVM, btagSF, btagSFerr );
-     
-    weight      *= btagSF;
-    weightnoISR *= btagSF;
-
-    std::string hh = slep + sHT + "_" + signal_region + "_" + sampletype;
-    std::string hhnolep  =  sHT + "_" + signal_region + "_" + sampletype;//needed for genlept filling
-
-    if(recoedele||recoedmuo) histos[(string)"RecoLepEvents"        + hh]->Fill(fMT2tree->misc.MT2, weight);//no bg, lepton
-    if(recoedele||recoedmuo) histos[(string)"noISR_RecoLepEvents"        + hh]->Fill(fMT2tree->misc.MT2, weightnoISR);//no bg, lepton
-    if(fbTagError && !fMT2tree->misc.isData && btagSF!=0){//data has no BTV SF weights
-    if(recoedele||recoedmuo) histos[(string)"RecoLepEventsUp"         + hh]->Fill(fMT2tree->misc.MT2, weight/btagSF*(btagSF+btagSFerr));
-    if(recoedele||recoedmuo) histos[(string)"RecoLepEventsDown"       + hh]->Fill(fMT2tree->misc.MT2, weight/btagSF*(btagSF-btagSFerr));
-    if(recoedele||recoedmuo) histos[(string)"noISR_RecoLepEventsUp"   + hh]->Fill(fMT2tree->misc.MT2, weightnoISR/btagSF*(btagSF+btagSFerr));
-    if(recoedele||recoedmuo) histos[(string)"noISR_RecoLepEventsDown" + hh]->Fill(fMT2tree->misc.MT2, weightnoISR/btagSF*(btagSF-btagSFerr));
-    }
-    
-    if(recoedele||recoedmuo) histos[(string)"LeptonEvents"               + hh     ]->Fill(fMT2tree->misc.MT2,weight);//no bg, lepton
-    if(recoedelenomt)        histos[(string)"LeptonEventsNoMT_Ele"       + hhnolep]->Fill(fMT2tree->misc.MT2,weight);//no bg, lepton <-- here I need hhnolep
-    if(recoedmuonomt)        histos[(string)"LeptonEventsNoMT_Muo"       + hhnolep]->Fill(fMT2tree->misc.MT2,weight);//no bg, lepton <-- here I need hhnolep
-    if(recoedele||recoedmuo) histos[(string)"noISR_LeptonEvents"         + hh     ]->Fill(fMT2tree->misc.MT2,weightnoISR);//no bg, lepton
-    if(recoedelenomt)        histos[(string)"noISR_LeptonEventsNoMT_Ele" + hhnolep]->Fill(fMT2tree->misc.MT2,weightnoISR);//no bg, lepton <-- here I need hhnolep
-    if(recoedmuonomt)        histos[(string)"noISR_LeptonEventsNoMT_Muo" + hhnolep]->Fill(fMT2tree->misc.MT2,weightnoISR);//no bg, lepton <-- here I need hhnolep
-
-  } // for entries
-
-} 
-
-
-
-
-
-
 
 float getISRCorrection( MT2tree* fMT2tree, const MT2Sample& sample, const std::string& sampletype ) {
 
